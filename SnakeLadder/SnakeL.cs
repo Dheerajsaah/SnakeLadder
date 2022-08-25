cbd690369e9ab63441fae016a9a9bf3bb42e2ad9
dherajsaah <dheerajsaah248@gmail.com>
2022-08-25 17:18:12 +0530

    [Dheeraj] Add: CheckOption

--- SnakeLadder/SnakeL.cs
+++ SnakeLadder/SnakeL.cs
@@ -9,11 +9,29 @@ namespace SnakeLadder
     internal class SnakeL
     {
        int PlayerPosition = 0;
+       const int NO_PLAY = 0, LADDER = 1, SNAKE = 2; 
+       Random random = new Random();
         public int DiceRoll() 
         {
-            Random random = new Random();
-            int diceCount=random.Next(1, 7);
+            int diceCount = random.Next(1, 7);
             return diceCount;
+        }
+        public void Play()
+        {
+            int Option = random.Next(0, 3);
+            switch (Option)
+            {
+                case NO_PLAY:
+                    PlayerPosition += 0;
+                    break;
+                case LADDER:
+                    PlayerPosition +=DiceRoll();
+                    break;
+                case SNAKE:
+                    PlayerPosition -=DiceRoll();
+                    break;
+            }
+            Console.WriteLine(PlayerPosition);
         }
     }
 }
